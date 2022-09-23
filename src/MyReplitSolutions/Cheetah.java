1d2ff9964da2d78e630405608835dbbced306ba6
sarakara <gulcinsarankara@hotmail.com>
2022-09-23 18:16:46 -0400

    Replit Solutions

---
+++ src/MyReplitSolutions/Cheetah.java
@@ -0,0 +1,21 @@
+package MyReplitSolutions;
+
+public class Cheetah {
+    public static void main(String[] args) {
+           new Mammal(5);
+
+
+    }
+}
+class Pla extends Mammal{
+    public Pla(){
+
+        System.out.println("pla ");
+    }
+}
+    class Mammal{
+    public Mammal(int age){
+
+        System.out.println("Class Y");
+        }
+}
